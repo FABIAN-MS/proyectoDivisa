9e38c4a3ee44e94fa9d2ab1c01c96e2aeec3e7d1
Fabian MS <sanchezfabian607@gmail.com>
2025-02-16 22:03:21 -0600

    Correccion

--- app/src/main/java/com/example/proyectodivisas/retrofit/ExchangeRateService.kt
+++ app/src/main/java/com/example/proyectodivisas/retrofit/ExchangeRateService.kt
@@ -8,6 +8,6 @@ import retrofit2.http.Path
 
 interface ExchangeRateService {
 
-    @GET("v6/c05011d4ff8ab7037ac5946b/latest/{baseCode}")
+    @GET("v6/834635aa2b5f8b8837fc15fc/latest/{baseCode}")
     suspend fun getLatestRates(@Path("baseCode") baseCode: String): Response<ExchangeRateResponse>
 }
